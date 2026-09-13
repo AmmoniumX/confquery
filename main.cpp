7f87fb3a5816a3979407d28003b9e31d64aa058c
Diego Garcia <diego.garcia.cr@gmail.com>
2026-09-12 20:23:03 -0600

    update argdispatcher submodule and usage

--- main.cpp
+++ main.cpp
@@ -94,40 +94,55 @@ void set_key_value_op(string_view file, string_view section, string_view key,
 } // namespace
 
 int main(int argc, char *argv[]) {
-  argdispatch::ArgDispatcher dispatcher;
+  // clang-format off
+  argdispatch::ArgDispatcher dispatcher({
+      .program_name = "confquery",
+      .version      = "1.0.0",
+      .description  = "A command-line tool for querying and modifying configuration files.",
+  });
+  // clang-format on
 
+  dispatcher.register_builtin_commands();
   auto file = dispatcher.and_then<string_view>("file");
 
-  file.literal("-Qs").and_then<string_view>("section").executes(query_section);
+  // clang-format off
+  file.literal("-Qs")
+      .and_then<string_view>("section")
+      .executes(query_section, "query a section from the configuration file");
   file.literal("-Qv")
       .and_then<string_view>("section")
       .and_then<string_view>("value")
-      .executes(query_value);
+      .executes(query_value, "query a value from a specific section");
   file.literal("-Qk")
       .and_then<string_view>("section")
       .and_then<string_view>("key")
-      .executes(query_key);
-  file.literal("-Rs").and_then<string_view>("section").executes(remove_section_op);
+      .executes(query_key, "query a key-value pair from a specific section");
+  file.literal("-Rs")
+      .and_then<string_view>("section")
+      .executes(remove_section_op,"remove an entire section from the configuration file");
   file.literal("-Rv")
       .and_then<string_view>("section")
       .and_then<string_view>("value")
-      .executes(remove_value_op);
+      .executes(remove_value_op, "remove a value from a specific section");
   file.literal("-Rk")
       .and_then<string_view>("section")
       .and_then<string_view>("key")
-      .executes(remove_key_op);
+      .executes(remove_key_op, "remove a key-value pair from a specific section");
   file.literal("-Sv")
       .and_then<string_view>("section")
       .and_then<string_view>("value")
-      .executes(set_value_op);
+      .executes(set_value_op, "set a value in a specific section");
   file.literal("-Sk")
       .and_then<string_view>("section")
       .and_then<string_view>("key")
       .and_then<string_view>("value")
-      .executes(set_key_value_op);
+      .executes(set_key_value_op, "set a key-value pair in a specific section");
+  // clang-format on
+
+  auto dis = std::move(dispatcher).build();
 
   try {
-    return dispatcher.dispatch(argc, argv);
+    return dis.dispatch(argc, argv);
   } catch (const std::runtime_error &e) {
     std::println(std::cerr, "Internal error: {}", e.what());
     return std::to_underlying(ExitCode::EINTERNAL);
